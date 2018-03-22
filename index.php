b1b47de4a67e29059a17b8fbd2af89e65cfebdc8
Alex <chesnochenko.a@gmail.com>
2018-03-23 10:56:37 +1300

    Add 2 structures

---
+++ index.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>STD621_IP_Project</title>
+
+</head>
+<body>
+
+<h1>Structure 1</h1>
+<div class="basicStructure">
+
+    <!--    User functional    -->
+    <div class="header clearfix">
+        <ul>
+            <li>Logo</li>
+            <li>Rent a car <span>(main page)</span></li>
+            <li>Contact us</li>
+            <li>Log in / Sign up <span>OR</span> Log out</li>
+            <li>My account <span>(for logined users)</span></li>
+        </ul>
+    </div>
+
+    <div class="carCard clearfix">
+
+        <div class="row clearfix">
+            <div class="car">
+                <img  src="img/avensis.jpg" alt="vehicle">
+                <div class="description">Toyota Avensis</div>
+            </div>
+            <div class="car">
+                <img src="img/camry.jpg" alt="vehicle">
+                <div class="description">Toyota Camry</div>
+            </div>
+            <div class="car">
+                <img src="img/mustang.jpg" alt="vehicle">
+                <div class="description">Ford Mustang</div>
+            </div>
+            <div class="car">
+                <img src="img/duster.jpeg" alt="vehicle">
+                <div class="description">Renault Duster</div>
+            </div>
+            <div class="car">
+                <img src="img/fiat%20panda.jpg" alt="vehicle">
+                <div class="description">Fiat Panda</div>
+            </div>
+        </div>
+
+        <div class="row clearfix">
+            <div class="car">
+                <img  src="img/audi.png" alt="vehicle">
+                <div class="description">Audi TT</div>
+            </div>
+            <div class="car">
+                <img src="img/audi%20a6.jpg" alt="vehicle">
+                <div class="description">Audi A6</div>
+            </div>
+            <div class="car">
+                <img src="img/bmw.jpeg" alt="vehicle">
+                <div class="description">BMW</div>
+            </div>
+            <div class="car">
+                <img src="img/volkswagen.jpg" alt="vehicle">
+                <div class="description">Volkswagen Passat</div>
+            </div>
+            <div class="car">
+                <img src="img/maserati.jpg" alt="vehicle">
+                <div class="description">Maserati</div>
+            </div>
+        </div>
+
+    </div>
+
+</div>
+
+<h1>Structure 2</h1>
+<div class="basicStructure">
+
+    <!--    User functional    -->
+    <div class="header clearfix">
+        <ul>
+            <li>Logo</li>
+            <li id="floatRight">Log in / Sign up <span>OR</span> Log out</li>
+            <li id="floatRight">My account <span>(for logined users)</span></li>
+        </ul>
+    </div>
+
+    <div class="carCard clearfix">
+
+        <div class="row clearfix">
+            <div class="car">
+                <img  src="img/avensis.jpg" alt="vehicle">
+                <div class="description">Toyota Avensis</div>
+            </div>
+            <div class="car">
+                <img src="img/camry.jpg" alt="vehicle">
+                <div class="description">Toyota Camry</div>
+            </div>
+            <div class="car">
+                <img src="img/mustang.jpg" alt="vehicle">
+                <div class="description">Ford Mustang</div>
+            </div>
+            <div class="car">
+                <img src="img/duster.jpeg" alt="vehicle">
+                <div class="description">Renault Duster</div>
+            </div>
+            <div class="car">
+                <img src="img/fiat%20panda.jpg" alt="vehicle">
+                <div class="description">Fiat Panda</div>
+            </div>
+        </div>
+
+        <div class="row clearfix">
+            <div class="car">
+                <img  src="img/audi.png" alt="vehicle">
+                <div class="description">Audi TT</div>
+            </div>
+            <div class="car">
+                <img src="img/audi%20a6.jpg" alt="vehicle">
+                <div class="description">Audi A6</div>
+            </div>
+            <div class="car">
+                <img src="img/bmw.jpeg" alt="vehicle">
+                <div class="description">BMW</div>
+            </div>
+            <div class="car">
+                <img src="img/volkswagen.jpg" alt="vehicle">
+                <div class="description">Volkswagen Passat</div>
+            </div>
+            <div class="car">
+                <img src="img/maserati.jpg" alt="vehicle">
+                <div class="description">Maserati</div>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="contactus">
+        <img src="img/consultant.png" alt="consultant">
+        <div class="description">Write us a message</div>
+        <div class="close">X</div>
+    </div>
+
+</div>
+
+<script src="js/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/script.js"></script>
+</body>
+</html>
